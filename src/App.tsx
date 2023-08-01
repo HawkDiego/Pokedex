7d52f307c25c13fa5552d3210905da95ab0f78cd
HawkDiego <judgiraldogo@unal.edu.co>
2023-08-01 14:43:33 -0500

    new proyect

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,48 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Result, Pokemon } from './types/pokemon'
 
 function App() {
-  const [pokemons, setPokemons] = useState<Pokemon[]>()
-  const [number, setNumber] = useState(0)
-
-  const sortByPosition = (a: Pokemon, b: Pokemon) => {
-    return a.order - b.order
-  }
-
-  const fetching = async () => {
-    try {
-      const promise = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
-      const res = await promise.json()
-      const data: Result[] = res.results
-      let pokemonList: Pokemon[] = []
-
-      await Promise.all(
-        data.map(async (pokemon) => {
-          const pokemonDataPromise = await fetch(`${pokemon.url}`)
-          const pokemonData: Pokemon = await pokemonDataPromise.json()
-          pokemonList.push(pokemonData)
-        })
-      )
-      pokemonList.sort(sortByPosition)
-      setPokemons(pokemonList)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const handleClickMore = () => {
-    setNumber((prevState) => prevState + 1)
-  }
-  const handleClickLess = () => {
-    setNumber((prevState) => {
-      if (prevState > 0) {
-        return prevState - 1
-      } else {
-        return prevState
-      }
-    })
-  }
-
-  useEffect(() => {
-    fetching()
-  }, [fetching])
+  const [count, setCount] = useState(0)
 
   return (
-    <body>
-      <div className='pokedex'>
-        <div className='header'>
-          <h1>Pokédex</h1>
-        </div>
-        {pokemons ? (
-          <div className='screen'>
-            <img
-              src={`${pokemons[number].sprites.front_default}`}
-              alt='Pokémon'
-              className='pokemon-image'
-            />
-
-            <h2 className='pokemon-name'>{pokemons[number].name}</h2>
-            <p className='pokemon-info'>
-              Pokémon de tipo planta y veneno. Su bulbo en la espalda crece a
-              medida que lo hace él.
-            </p>
-          </div>
-        ) : undefined}
-
-        <div className='buttons'>
-          <button
-            className='prev-button'
-            onClick={handleClickLess}
-          >
-            ←
-          </button>
-          <button
-            className='next-button'
-            onClick={handleClickMore}
-          >
-            →
-          </button>
-        </div>
+    <>
+      <div>
+        <a
+          href='https://vitejs.dev'
+          target='_blank'
+        >
+          <img
+            src={viteLogo}
+            className='logo'
+            alt='Vite logo'
+          />
+        </a>
+        <a
+          href='https://react.dev'
+          target='_blank'
+        >
+          <img
+            src={reactLogo}
+            className='logo react'
+            alt='React logo'
+          />
+        </a>
       </div>
-    </body>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
